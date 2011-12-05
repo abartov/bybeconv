f5e5c269a6e3d93937862ee5bc458b7a31c0e737
Asaf Bartov <asaf.bartov@gmail.com>
2011-12-05 00:39:14 -0800

    - started analysis method -- still to figure out how to handle encoding errors in _source_ files

--- app/models/html_file.rb
+++ app/models/html_file.rb
@@ -1,2 +1,16 @@
 class HtmlFile < ActiveRecord::Base
+
+  def analyze
+  
+    # Word footnotes magic word 'mso-footnote-id'
+    html = File.open(self.path, "r:ISO-8859-8:UTF-8").read
+    if html =~ /mso-footnote-id/
+      self.footnotes = true
+    end
+    print "Analysis results -- footnotes: #{self.footnotes}, images: #{self.images}, tables: #{self.tables}\n"
+    self.save!
+    # Word tables magic word (beyond the basic BY formatting table for prose!): 
+    # Word images magic word: 
+  end
+
 end
