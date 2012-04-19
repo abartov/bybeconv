3a7baacb75ba20927f271c0b98be67961ca1a46f
Asaf Bartov <asaf.bartov@gmail.com>
2012-04-19 04:14:31 +0300

    - updated to support deployment and MySQL server _

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120409045525) do
+ActiveRecord::Schema.define(:version => 20120419004222) do
 
   create_table "html_files", :force => true do |t|
     t.string   "path"
@@ -32,5 +32,15 @@ ActiveRecord::Schema.define(:version => 20120409045525) do
   end
 
   add_index "html_files", ["path"], :name => "index_html_files_on_path"
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
 end
