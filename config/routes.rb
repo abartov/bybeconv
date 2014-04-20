c9427a31e86377e58454f68fe7058c63ee647551
Asaf Bartov <asaf.bartov@gmail.com>
2014-04-19 18:56:13 -0700

    Added auth and welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Bybeconv::Application.routes.draw do
+  get "welcome/index"
+
   get "session/create"
   get "session/destroy"
   get "session/login"
@@ -62,7 +64,7 @@ Bybeconv::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
