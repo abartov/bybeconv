6a8944d62bfe788a720058fb039dbe7cf2cc6ee1
Asaf Bartov <asaf.bartov@gmail.com>
2014-05-04 16:32:37 -0700

    improved list_for_editor display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,11 @@ module ApplicationHelper
     return s if s.nil?
     return s.force_encoding('UTF-8')
   end
+  def url_tag(u)
+    return '' if u.blank?
+    return 'ויקיפדיה' if u =~ /https?:\/\/he.wikipedia.org/
+    return 'לקסיקון' if u =~ /https?:\/\/library.osu.edu\/projects\/hebrew-lexicon/ 
+    return 'VIAF' if u =~ /https?:\/\/viaf.org\/viaf\//
+    return 'אחר' 
+  end
 end
