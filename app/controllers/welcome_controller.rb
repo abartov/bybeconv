693159cd93e66c2ba756710ca1b7021d18bad3c0
Asaf Bartov <asaf.bartov@gmail.com>
2014-10-13 11:39:43 -0700

    Added links from editor dashboard to new proof and recommendation functionality.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    @open_proofs = Proof.where(status: 'new').count.to_s
+    @open_recommendations = Recommendation.where(status: 'new').count.to_s
   end
 end
