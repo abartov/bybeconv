b8c6f7f1cf5cc2b274b2c91451a8669b24d94e46
Asaf Bartov <asaf.bartov@gmail.com>
2015-07-05 00:42:54 -0700

    commit before archiving 1255 stuff in a branch

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,8 @@ ActiveRecord::Schema.define(:version => 20141013043434) do
   create_table "html_dirs", :force => true do |t|
     t.string   "path"
     t.string   "author"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
     t.boolean  "need_resequence"
   end
 
@@ -26,8 +26,8 @@ ActiveRecord::Schema.define(:version => 20141013043434) do
     t.string   "url"
     t.string   "status"
     t.string   "problem"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",          :null => false
+    t.datetime "updated_at",          :null => false
     t.boolean  "tables"
     t.boolean  "footnotes"
     t.boolean  "images"
@@ -54,8 +54,8 @@ ActiveRecord::Schema.define(:version => 20141013043434) do
     t.text     "what"
     t.boolean  "subscribe"
     t.string   "status"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
     t.integer  "html_file_id"
     t.integer  "resolved_by"
   end
@@ -75,8 +75,8 @@ ActiveRecord::Schema.define(:version => 20141013043434) do
   create_table "sessions", :force => true do |t|
     t.string   "session_id", :null => false
     t.text     "data"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
