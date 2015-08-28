34e638cccd05afbcfea7781219235fb0e9218e15
Asaf Bartov <asaf.bartov@gmail.com>
2015-08-28 02:21:55 -0700

    Enable CORS from any domain (workaround)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   after_filter :set_access_control_headers
 
   def set_access_control_headers 
-    headers['Access-Control-Allow-Origin'] = 'http://benyehuda.org/' 
+    headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Request-Method'] = '*' 
   end
 
