23ba3f6335dc09a74c5cc6488021d2e14dad7a6a
Asaf Bartov <asaf.bartov@gmail.com>
2015-11-02 23:51:56 +0000

    Switched to UTF8 for strip_nikkud

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   after_filter :set_access_control_headers
 
   def set_access_control_headers 
-    headers['Access-Control-Allow-Origin'] = 'http://benyehuda.org/' 
-    headers['Access-Control-Request-Method'] = '*' 
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = '*' 
   end
 
   private
