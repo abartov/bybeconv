4e4e20a2c80ed0c0a33fd5bb65a71486bff5c17a
Asaf Bartov <asaf.bartov@gmail.com>
2016-04-08 12:09:44 -0700

    implemented rudimentary get_markdown API

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,4 +1,26 @@
 class ApiController < ApplicationController
+  respond_to :json
+  before_filter :ensure_json_request  
+
+  def ensure_json_request  
+    return if request.format == :json
+    render :nothing => true, :status => 406  
+  end 
+
   def query
+    # TODO: add API key validation
+    case params[:api_action]
+    when 'get_markdown'
+      # TODO: replace this with serving from Expressions rather than HtmlFiles
+      the_url = params[:path]
+      the_url = '/'+the_url if the_url[0] != '/' # prepend slash if necessary
+      h = HtmlFile.find_by_url(the_url)
+      unless h.nil?
+        markdown = File.open(h.path+'.markdown')
+        render json: { markdown: markdown }
+      end
+    else
+      render json: "ERROR: Unsupported API action #{params[:action]}"
+    end
   end
 end
