9a46e39f0cd61850f3708209501ac5ffe2090ef4
Asaf Bartov <asaf.bartov@gmail.com>
2016-06-22 04:32:52 -0700

    more stubbing of put_markdown

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -24,8 +24,19 @@ class ApiController < ApplicationController
         end
       end
     when 'put_markdown'
-      # TODO: implement
-      render :nothing => true, :status => 200
+      # TODO: add API key validation with WRITE access
+      the_url = params[:path]
+      the_url = '/'+the_url if the_url[0] != '/' # prepend slash if necessary
+      h = HtmlFile.find_by_url(the_url)
+      unless h.nil?
+        # TODO: update markdown in manifestation
+        respond_to do |fmt|
+          fmt.html { render text: 'OK' }
+          fmt.json { render json: 'OK' }
+        end
+      else
+        render :nothing => true, :status => 200
+      end
     else
       render json: "ERROR: Unsupported API action #{params[:action]}"
     end
