3a0ccedb3e0d32be60c470c8224b4d9d17452ca3
Asaf Bartov <asaf.bartov@gmail.com>
2016-09-15 12:56:22 +0300

    Added ExternalLinks to manifestations

--- app/models/manifestation.rb
+++ app/models/manifestation.rb
@@ -2,6 +2,7 @@ class Manifestation < ActiveRecord::Base
   has_and_belongs_to_many :expressions
   has_and_belongs_to_many :people
   has_paper_trail
+  has_many :external_links
 
   def long?
     return false # TODO: implement
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20160915085325) do
+ActiveRecord::Schema.define(:version => 20160915095315) do
 
   create_table "api_keys", :force => true do |t|
     t.string   "email"
@@ -51,6 +51,15 @@ ActiveRecord::Schema.define(:version => 20160915085325) do
   create_table "expressions_works", :id => false, :force => true do |t|
     t.integer "expression_id"
     t.integer "work_id"
+  end
+
+  create_table "external_links", :force => true do |t|
+    t.string   "url"
+    t.string   "linktype"
+    t.integer  "status"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
+    t.integer  "manifestation_id"
   end
 
   create_table "html_dirs", :force => true do |t|
