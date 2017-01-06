931359ab5e6da0535d38aa2ba2581a351a8885e5
Asaf Bartov <asaf.bartov@gmail.com>
2017-01-05 16:18:26 -0800

    Pagination for people

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,7 +2,7 @@ class PeopleController < ApplicationController
   # GET /people
   # GET /people.json
   def index
-    @people = Person.all
+    @people = Person.page(params[:page]).order('name asc')
 
     respond_to do |format|
       format.html # index.html.erb
