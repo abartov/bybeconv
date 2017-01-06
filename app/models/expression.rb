89f43c6f49a51aad010e33694b2ad300181e3691
Asaf Bartov <asaf.bartov@gmail.com>
2017-01-05 22:12:28 -0800

    Auto-updating translation boolean

--- app/models/expression.rb
+++ app/models/expression.rb
@@ -1,5 +1,18 @@
 class Expression < ActiveRecord::Base
+  before_save :set_translation
+
   has_and_belongs_to_many :works
   has_and_belongs_to_many :manifestations
   has_and_belongs_to_many :people
+
+  def determine_is_translation?
+    # determine whether this expression is a translation or not, i.e. is in a different language to the work it expresses
+    language != works[0].orig_lang # TODO: handle multiple works?
+  end
+
+  protected
+  def set_translation
+    self.translation = determine_is_translation?
+    return true
+  end
 end
