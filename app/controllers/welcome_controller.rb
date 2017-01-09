a4bb3be38b1852429475e82c7fd52ad208849387
Asaf Bartov <asaf.bartov@gmail.com>
2017-01-08 22:56:36 -0800

    Implemented basic whatsnew view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,7 +6,7 @@ class WelcomeController < ApplicationController
     @totals = { works: get_total_works, authors: get_total_authors, headwords: get_total_headwords }
     @pop_authors = popular_authors.map{|a| a[0]} # we don't need the actual counts here
     @random_authors = randomize_authors(@pop_authors)
-
+    @whatsnew = whatsnew_anonymous # TODO: custom calculate for logged-in users
     @pagetype = :homepage
     @page_title = t(:default_page_title)+' - '+t(:homepage)
   end
