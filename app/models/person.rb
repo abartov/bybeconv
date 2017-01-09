13b51ac0e7d31c84a468b3c39dcef44e81413264
Asaf Bartov <asaf.bartov@gmail.com>
2017-01-08 19:30:22 -0800

    Added has_toc scope

--- app/models/person.rb
+++ app/models/person.rb
@@ -7,6 +7,7 @@ class Person < ActiveRecord::Base
   has_and_belongs_to_many :work
   has_and_belongs_to_many :expression
   has_and_belongs_to_many :manifestation
+  scope :has_toc, -> { where.not(toc_id: nil) }
 
   has_attached_file :profile_image, styles: { full: "720x1040", medium: "360x520", thumb: "180x260"}, default_url: "/images/:style/missing.png", storage: :s3, s3_credentials: 'config/s3.yml', s3_region: 'us-east-1'
   validates_attachment_content_type :profile_image, content_type: /\Aimage\/.*\z/
