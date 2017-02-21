cb37113cffc26bcc743d644e4490d2f0ce1b1573
Asaf Bartov <asaf.bartov@gmail.com>
2017-02-21 13:40:48 -0800

    css fixes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,6 +5,7 @@ class WelcomeController < ApplicationController
   def index
     @totals = { works: get_total_works, authors: get_total_authors, headwords: get_total_headwords }
     @pop_authors = popular_authors.map{|a| a[0]} # we don't need the actual counts here
+    @pop_authors_this_month = @pop_authors # Temporary hack! TODO: stop cheating and actually count by month
     @pop_works = popular_works.map{|a| a[0]} # ditto
     @random_authors = randomize_authors(@pop_authors)
     @random_works = randomize_works(4) # TODO: un-hardcode?
