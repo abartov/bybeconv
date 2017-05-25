5bc9f55a106f4839e728f7ace68d57e4a01889a7
Asaf Bartov <asaf.bartov@gmail.com>
2017-05-25 09:58:41 -0700

    Added index on created_at for manifestations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170525163157) do
+ActiveRecord::Schema.define(version: 20170525165813) do
 
   create_table "api_keys", force: :cascade do |t|
     t.string   "email",       limit: 255
@@ -189,6 +189,8 @@ ActiveRecord::Schema.define(version: 20170525163157) do
     t.datetime "updated_at",                                null: false
     t.text     "markdown",                 limit: 16777215
   end
+
+  add_index "manifestations", ["created_at"], name: "index_manifestations_on_created_at", using: :btree
 
   create_table "manifestations_people", id: false, force: :cascade do |t|
     t.datetime "created_at",                 null: false
