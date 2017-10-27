5b302638731ca1038bfeda0234924052c6d91112
Asaf Bartov <asaf.bartov@gmail.com>
2017-10-27 15:09:47 +0300

    Fixed missing language

--- lib/bybe_utils.rb
+++ lib/bybe_utils.rb
@@ -288,7 +288,7 @@ module BybeUtils
   end
 
   def get_langs
-    return ['he','en','fr','de','ru','yi','pl','ar','el','la','grc','hu','cs','da','no','sv','nl']
+    return ['he','en','fr','de','ru','yi','pl','ar','el','la','grc','hu','cs','da','no','sv','nl','it']
   end
 
   def get_genres_by_row(row) # just one row at a time
