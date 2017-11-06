89baa1a92213e34d39249e503c4c379568ea976f
Asaf Bartov <asaf.bartov@gmail.com>
2017-11-06 11:15:33 +0100

    Re-enable legacy routes

--- config/routes.rb
+++ config/routes.rb
@@ -163,5 +163,5 @@ Bybeconv::Application.routes.draw do
   # match ':controller(/:action(/:id(.:format)))'
   #
   # match legacy BY urls
-  # tmp # match '*path' => "html_file#render_by_legacy_url", via: [:get]
+  match '*path' => "html_file#render_by_legacy_url", via: [:get]
 end
