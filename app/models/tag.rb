009fc4e99a1e6fa27c1a89fd42d2994ffd82f2ab
Asaf Bartov <asaf.bartov@gmail.com>
2017-11-28 02:21:27 -0800

    Tag statuses and scopes

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,9 @@ class Tag < ActiveRecord::Base
   attr_accessible :created_by, :name, :status
   has_many :taggings
   has_many :manifestations, through: :taggings, :class_name => 'Manifestation'
+  enum status: [:pending, :approved]
+
+  scope :pending, -> { where(status: Tag.statuses[:pending]) }
+  scope :approved, -> { where(status: Tag.statuses[:approved]) }
+
 end
