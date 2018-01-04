448606de4d94e54b50ff8b322ecf28467bc68d7b
Asaf Bartov <asaf.bartov@gmail.com>
2018-01-03 16:34:23 -0800

    fixed static page plain markdown rendering.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -130,7 +130,7 @@ class AdminController < ApplicationController
       redirect_to url_for(action: :index)
     end
     @markdown = MultiMarkdown.new(@vp.body).to_html.force_encoding('UTF-8')
-    unless @vp.mode == :plain_markdown # then all paragraphs should be in cards
+    unless @vp.mode == 'plain_markdown' # then all paragraphs should be in cards
       parts = @markdown.split("<h2")
       newbuf = parts[0]
       parts[1..-1].each {|p|
