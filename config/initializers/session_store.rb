34816e49815c5c1616041271186db11b6a0c48cc
Asaf Bartov <asaf.bartov@gmail.com>
2019-01-18 22:47:40 +0200

    Keeps sessions for two months

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,4 +5,4 @@
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-Bybeconv::Application.config.session_store :active_record_store
+Bybeconv::Application.config.session_store :active_record_store, expire_after: 60.days
