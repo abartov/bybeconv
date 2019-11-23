6d529f03ed3b82d6a28c50b208e1832cab06d652
Asaf Bartov <asaf.bartov@gmail.com>
2019-11-24 01:47:32 +0200

    Add an auto-updating sorting_title field to manifestations

---
+++ db/migrate/20191123194352_add_sort_title_to_manifestation.rb
@@ -0,0 +1,16 @@
+class AddSortTitleToManifestation < ActiveRecord::Migration[5.2]
+  def change
+    add_column :manifestations, :sort_title, :string
+    add_index :manifestations, :sort_title
+
+    puts "Populating sort_title column..."
+    total = Manifestation.count
+    i = 0
+    Manifestation.all.each do |m|
+      puts "#{i}/#{total} done" if i % 100 == 0
+      m.update_sort_title
+      m.save
+      i += 1
+    end
+  end
+end
