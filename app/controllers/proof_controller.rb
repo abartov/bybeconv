97c73442c790d761bdcc914366e9430f555f2270
Asaf Bartov <asaf.bartov@gmail.com>
2019-12-02 21:22:01 +0200

    Added temporary filter for bad proofs

--- app/controllers/proof_controller.rb
+++ app/controllers/proof_controller.rb
@@ -17,8 +17,10 @@ class ProofController < ApplicationController
         end
       end
     else # new BYBE
-      @p = Proof.new(from: params['from'], manifestation_id: params['manifestation'].to_i, what: params['what'], highlight: params['highlight'], status: 'new')
-      @p.save!
+      if params['ziburit'] =~ /ביאליק/
+        @p = Proof.new(from: params['from'], manifestation_id: params['manifestation'].to_i, what: params['what'], highlight: params['highlight'], status: 'new')
+        @p.save!
+      end
     end
     respond_to do |fmt|
       fmt.html { }
