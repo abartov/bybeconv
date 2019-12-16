48193b64b7dfe1e68341cb40d24d4ec379b34477
Asaf Bartov <asaf.bartov@gmail.com>
2019-12-16 17:39:28 +0200

    Implemented *display* of period facet

--- app/controllers/manifestation_controller.rb
+++ app/controllers/manifestation_controller.rb
@@ -448,6 +448,7 @@ class ManifestationController < ApplicationController
       @collection = @collection.joins(expressions: :works).includes(expressions: :works) # make sure we have the joins we need
       @emit_filters = true
       @genre_facet = @collection.group('expressions.genre').count
+      @period_facet = @collection.group('period').count
       # TODO: other facets
     end
   end
