0eab42e2ae1e9a5d5f7494e8839746555c50c1cb
Asaf Bartov <asaf.bartov@gmail.com>
2019-12-24 23:51:35 +0200

    Added 'unknown' language, e.g. for translations without specified source language

--- lib/bybe_utils.rb
+++ lib/bybe_utils.rb
@@ -464,7 +464,7 @@ module BybeUtils
   end
 
   def get_langs
-    return ['he','en','fr','de','ru','yi','pl','ar','el','la','grc','hu','cs','da','no','sv','nl','it','pt','fa','fi','is','es']
+    return ['he','en','fr','de','ru','yi','pl','ar','el','la','grc','hu','cs','da','no','sv','nl','it','pt','fa','fi','is','es','unk']
   end
 
   def get_genres_by_row(row) # just one row at a time
