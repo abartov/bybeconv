7b5160f693979614e48049eb6cf6e9906f0d586d
Asaf Bartov <asaf.bartov@gmail.com>
2020-03-25 00:41:01 +0200

    Fixed issue #493 language facet disappearing

--- app/controllers/manifestation_controller.rb
+++ app/controllers/manifestation_controller.rb
@@ -589,6 +589,9 @@ class ManifestationController < ApplicationController
       @copyright_facet = make_collection(query_parts.reject{|k,v| k == :copyright }, query_params, joins_needed, ord).group(:copyrighted).count
       @language_facet = make_collection(query_parts.reject{|k,v| k == :languages }, query_params, joins_needed, ord).group('works.orig_lang').count
       @language_facet[:xlat] = @language_facet.values.sum - (@language_facet['he'] || 0)
+      get_langs.each do |l|
+        @language_facet[l] = 0 unless @language_facet[l].present?
+      end
       @uploaded_dates = make_collection(query_parts.reject{|k,v| k == :uploaded }, query_params, joins_needed, ord).pluck(:created_at).map{|x| "{value: #{x.strftime("%Y%m%d")}}"}.join(", ")
       # TODO: date histogram      # (bins, freqs) = uploaded_dates.histogram
       # TODO: curated facet
