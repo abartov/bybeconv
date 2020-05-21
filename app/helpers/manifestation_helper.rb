e554f35bc77373ea2c3e2ce372a3f266fdeafb57
Asaf Bartov <asaf.bartov@gmail.com>
2020-05-21 20:18:01 +0300

    Tolerate non-image uploads

--- app/helpers/manifestation_helper.rb
+++ app/helpers/manifestation_helper.rb
@@ -1,7 +1,8 @@
 module ManifestationHelper
   def options_from_images(images)
     # full resolution URL in value, thumbnail URL in imagesrc!
-    images.map{|img| "<option value=\"#{url_for(img)}\" data-imagesrc=\"#{request.base_url+url_for(img.variant(resize: '150x150'))}\">#{img.blob.filename}</option>"}.join('')
+    actual_images = images.reject{|img| !img.variable?} # skip any non-image attachments that may have been accidentally uploaded
+    return actual_images.map{|img| "<option value=\"#{url_for(img)}\" data-imagesrc=\"#{request.base_url+url_for(img.variant(resize: '150x150'))}\">#{img.blob.filename}</option>"}.join('')
   end
   def all_images_markdown(images)
     escape_javascript(images.map{|img| "\n![#{img.blob.filename}](#{url_for(img)})\n"}.join)
