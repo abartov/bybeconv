5aaa3e1841f27f39e82df06a7cf0cef6cc553eb2
Asaf Bartov <asaf.bartov@gmail.com>
2020-10-26 21:39:20 +0200

    Fixed startup error

--- app/models/dictionary_link.rb
+++ app/models/dictionary_link.rb
@@ -1,4 +1,4 @@
 class DictionaryLink < ApplicationRecord
-  belongs_to :from_entry, class: :DictionaryEntry
-  belongs_to :to_entry, class: :DictionaryEntry
+  belongs_to :from_entry, class_name: :DictionaryEntry
+  belongs_to :to_entry, class_name: :DictionaryEntry
 end
