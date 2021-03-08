a8c9e51fdfbd00094a9e683ba84cae0964e008d4
Asaf Bartov <asaf.bartov@gmail.com>
2021-03-08 13:42:14 +0200

    Fixed #536 static pages UI

--- app/models/static_page.rb
+++ app/models/static_page.rb
@@ -19,14 +19,14 @@ class StaticPage < ApplicationRecord
 
   def prepare_markdown
     markdown = MultiMarkdown.new(self.body).to_html.force_encoding('UTF-8')
-    unless self.mode == 'plain_markdown' # then all paragraphs should be in cards
-      parts = markdown.split("<h2")
-      newbuf = parts[0]
-      parts[1..-1].each {|p|
-        newbuf += '<div class="by-card-v02"><h2' + p + '</div>'
-      }
-      markdown = newbuf
-    end
+#    unless self.mode == 'plain_markdown' # then all paragraphs should be in cards
+#      parts = markdown.split("<h2")
+#      newbuf = parts[0]
+#      parts[1..-1].each {|p|
+#        newbuf += '<div class="by-card-v02"><h2' + p + '</div>'
+#      }
+#      markdown = newbuf
+#    end
     return markdown
   end
 end
