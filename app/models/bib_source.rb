33a4b95c8014b0085596a1808b77e5ad38ad271e
Asaf Bartov <asaf.bartov@gmail.com>
2021-05-14 11:28:32 +0300

    Implemented NLI API for bib workshop

--- app/models/bib_source.rb
+++ app/models/bib_source.rb
@@ -1,7 +1,7 @@
 class BibSource < ApplicationRecord
   has_many :holdings
   has_many :publications
-  enum source_type: [:aleph, :primo, :idea, :hebrewbooks, :googlebooks] # according to the types supported by the Gared gem
+  enum source_type: [:aleph, :primo, :idea, :hebrewbooks, :googlebooks, :nli_api] # according to the types supported by the Gared gem
   enum status: [:enabled, :disabled]
 
   scope :physical_sources, -> { where(status: 'enabled', source_type: [BibSource.source_types[:aleph], BibSource.source_types[:primo], BibSource.source_types[:idea]])}
