b786be45c32356806b5a708c128849ed05bfc02a
Asaf Bartov <asaf.bartov@gmail.com>
2021-07-21 01:31:29 +0300

    Emit dates in list when sorting by date

--- app/helpers/manifestation_helper.rb
+++ app/helpers/manifestation_helper.rb
@@ -10,4 +10,30 @@ module ManifestationHelper
   def all_images_markdown(images)
     escape_javascript(images.map{|img| "\n![#{img.blob.filename}](#{url_for(img)})\n"}.join)
   end
+  def browse_item_decorator_by_sort_type(sort_type)
+    case sort_type
+    when /publ/
+      return method(:browse_pub_date)
+    when /cre/
+      return method(:browse_creation_date)
+    when /upl/
+      return method(:browse_upload_date)
+    else
+      return method(:browse_null_decorator)
+    end
+  end
+  def browse_pub_date(item)
+    thedate = item.expressions[0].normalized_pub_date
+    return " (#{thedate.nil? ? t(:unknown) : thedate.to_date.strftime('%d-%m-%Y')})"
+  end
+  def browse_creation_date(item)
+    thedate = item.expressions[0].works[0].normalized_creation_date
+    return " (#{thedate.nil? ? t(:unknown) : thedate.to_date.strftime('%d-%m-%Y')})"
+  end
+  def browse_upload_date(item)
+    return " (#{item.created_at.strftime('%d-%m-%Y')})"
+  end
+  def browse_null_decorator(item)
+    return ''
+  end
 end
