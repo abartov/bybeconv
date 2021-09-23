be25c66e6006237026a76b0aa50964baa7c0c3f4
Asaf Bartov <asaf.bartov@gmail.com>
2021-09-23 17:12:48 +0300

    Also include new nli_api in shopping lists

--- app/models/bib_source.rb
+++ app/models/bib_source.rb
@@ -4,6 +4,6 @@ class BibSource < ApplicationRecord
   enum source_type: [:aleph, :primo, :idea, :hebrewbooks, :googlebooks, :nli_api] # according to the types supported by the Gared gem
   enum status: [:enabled, :disabled]
 
-  scope :physical_sources, -> { where(status: 'enabled', source_type: [BibSource.source_types[:aleph], BibSource.source_types[:primo], BibSource.source_types[:idea]])}
+  scope :physical_sources, -> { where(status: 'enabled', source_type: [BibSource.source_types[:aleph], BibSource.source_types[:primo], BibSource.source_types[:nli_api], BibSource.source_types[:idea]])}
 
 end
