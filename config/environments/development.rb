fedad886c811a90952aac4f1257959a1eea3c1cf
Asaf Bartov <asaf.bartov@gmail.com>
2021-12-15 02:40:43 +0200

    UI fixes following CSS merge

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,24 +70,24 @@ Bybeconv::Application.configure do
     config.action_view.cache_template_loading = true
   end
   #### Bullet settings for performance optimization
-  config.after_initialize do
-    Bullet.enable = true
-    #Bullet.sentry = true
-    Bullet.alert = true
-    Bullet.bullet_logger = true
-    Bullet.console = true
-    Bullet.growl = false
-    #Bullet.xmpp = { :account  => 'bullets_account@jabber.org',                     :password => 'bullets_password_for_jabber',                     :receiver => 'your_account@jabber.org',                     :show_online_status => true }
-    Bullet.rails_logger = true
-    Bullet.honeybadger = false
-    Bullet.bugsnag = false
-    Bullet.appsignal = false
-    Bullet.airbrake = false
-    Bullet.rollbar = false
-    Bullet.add_footer = true
-    Bullet.skip_html_injection = false
-    #Bullet.stacktrace_includes = [ 'your_gem', 'your_middleware' ]
-    #Bullet.stacktrace_excludes = [ 'their_gem', 'their_middleware', ['my_file.rb', 'my_method'], ['my_file.rb', 16..20] ]
-    #Bullet.slack = { webhook_url: 'http://some.slack.url', channel: '#default', username: 'notifier' }
-  end
+  # config.after_initialize do
+  #   Bullet.enable = true
+  #   #Bullet.sentry = true
+  #   Bullet.alert = true
+  #   Bullet.bullet_logger = true
+  #   Bullet.console = true
+  #   Bullet.growl = false
+  #   #Bullet.xmpp = { :account  => 'bullets_account@jabber.org',                     :password => 'bullets_password_for_jabber',                     :receiver => 'your_account@jabber.org',                     :show_online_status => true }
+  #   Bullet.rails_logger = true
+  #   Bullet.honeybadger = false
+  #   Bullet.bugsnag = false
+  #   Bullet.appsignal = false
+  #   Bullet.airbrake = false
+  #   Bullet.rollbar = false
+  #   Bullet.add_footer = true
+  #   Bullet.skip_html_injection = false
+  #   #Bullet.stacktrace_includes = [ 'your_gem', 'your_middleware' ]
+  #   #Bullet.stacktrace_excludes = [ 'their_gem', 'their_middleware', ['my_file.rb', 'my_method'], ['my_file.rb', 16..20] ]
+  #   #Bullet.slack = { webhook_url: 'http://some.slack.url', channel: '#default', username: 'notifier' }
+  # end
 end
