1135e1375850f57e4d54bbb7024cf6a65b28631e
Asaf Bartov <asaf.bartov@gmail.com>
2021-12-17 03:19:19 +0200

    Canonical project URL for API docs

--- config/initializers/rswag-ui.rb
+++ config/initializers/rswag-ui.rb
@@ -6,7 +6,7 @@ Rswag::Ui.configure do |c|
   # NOTE: If you're using rspec-api to expose Swagger files (under swagger_root) as JSON or YAML endpoints,
   # then the list below should correspond to the relative paths for those endpoints
 
-  c.swagger_endpoint 'http://localhost:3000/api/v1/swagger_doc', 'API V1 Docs'
+  c.swagger_endpoint 'https://benyehuda.org/api/v1/swagger_doc', 'API V1 Docs'
 
   # Add Basic Auth in case your API is private
   # c.basic_auth_enabled = true
