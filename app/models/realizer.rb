7fe3a77aa3a50e5566d839d57bd70d6c576ee15f
Damir Sultanbekov <Damir.Sultanbekov@toptal.com>
2022-01-08 15:17:15 +0500

    Removed author and adapter roles from Realizer table see https://github.com/abartov/bybeconv/issues/92

--- app/models/realizer.rb
+++ app/models/realizer.rb
@@ -2,10 +2,8 @@ class Realizer < ApplicationRecord
   belongs_to :expression
   belongs_to :person
   enum role: {
-    author: 0,
     editor: 1,
     illustrator: 2,
-    translator: 3,
-    adapter: 4
+    translator: 3
   }
 end
