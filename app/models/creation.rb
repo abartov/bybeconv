f1f4b5d0ed2feb362e1581772038dabb90490b7a
Damir Sultanbekov <Damir.Sultanbekov@toptal.com>
2022-01-10 20:13:37 +0500

    Removed editor role from creations table

--- app/models/creation.rb
+++ app/models/creation.rb
@@ -3,7 +3,6 @@ class Creation < ApplicationRecord
   belongs_to :person, class_name: 'Person', foreign_key: 'person_id'
   enum role: {
     author: 0,
-    editor: 1,
     illustrator: 2
   }
 end
--- spec/controllers/manifestations_controller_spec.rb
+++ spec/controllers/manifestations_controller_spec.rb
@@ -1,14 +1,16 @@
 require 'rails_helper'
 
 describe ManifestationController do
-  before(:all) do
-    clean_tables
-    Chewy.strategy(:atomic) do
-      create_list(:manifestation, 10)
-    end
-  end
+  let!(:manifestation) { create(:manifestation) }
 
-  describe 'Browse' do
+  describe 'browse' do
+    before(:all) do
+      clean_tables
+      Chewy.strategy(:atomic) do
+        create_list(:manifestation, 10)
+      end
+    end
+
     subject { get :browse, params: { sort_by: "#{sort_by}_#{sort_dir}" } }
 
     # Simply ensure all sort combinations works
@@ -17,6 +19,47 @@ describe ManifestationController do
         context "when #{dir} sorting by #{sort_by} is requested" do
           let(:sort_by) { sort_by }
           let(:sort_dir) { dir }
+          it { is_expected.to be_successful }
+        end
+      end
+    end
+
+    after(:all) do
+      clean_tables
+    end
+  end
+
+  describe 'read' do
+    subject { get :read, params: { id: manifestation.id } }
+
+    it { is_expected.to be_successful }
+  end
+
+  describe 'edit_metadata' do
+    subject { get :edit_metadata, params: { id: manifestation.id } }
+
+    context 'when user is not signed it' do
+      it { is_expected.to redirect_to('/') }
+    end
+
+    context 'when user is signed in' do
+      let(:user) { create(:user, editor: editor_flag) }
+
+      before do
+        session[:user_id] = user.id
+      end
+
+      context 'when user is not an editor' do
+        let(:editor_flag) { false }
+        it { is_expected.to redirect_to('/') }
+      end
+
+      context 'when user is an editor' do
+        let(:editor_flag) { true }
+        it { is_expected.to redirect_to('/') }
+
+        context 'when user has edit_catalog bit' do
+          let!(:edit_catalog_bit) { create(:list_item, item: user, listkey: :edit_catalog) }
           it { is_expected.to be_successful }
         end
       end
