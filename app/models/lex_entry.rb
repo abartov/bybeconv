0c9c905d7accbd8eac099a06664e0cd3d1b0f276
Asaf Bartov <asaf.bartov@gmail.com>
2022-05-30 00:40:18 +0300

    Person migration progress

--- app/models/lex_entry.rb
+++ app/models/lex_entry.rb
@@ -1,4 +1,7 @@
 class LexEntry < ApplicationRecord
+  has_one :lex_file
   belongs_to :lex_item, polymorphic: true
   # this can be LexPerson or LexPublication (or...?)
+  enum status: %i(raw migrated manual deprecated)
+
 end
