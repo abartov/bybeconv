80b00de65b43761f4730ab7b3649b5e102b44722
Asaf Bartov <asaf.bartov@gmail.com>
2023-01-21 06:07:55 +0200

    Tagging model specs

--- spec/factories/taggings.rb
+++ spec/factories/taggings.rb
@@ -1,4 +1,8 @@
 FactoryBot.define do
   factory :tagging do
+    status { :pending }
+    suggester { create(:user)}
+    tag { create(:tag)}
+    manifestation { create(:manifestation)}
   end
 end
