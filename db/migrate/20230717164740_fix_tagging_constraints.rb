aa435c24aeef416f76ae832815f343a15a987b8c
Asaf Bartov <asaf.bartov@gmail.com>
2023-07-28 19:18:07 +0300

    fix mysql syntax in migration

--- db/migrate/20230717164740_fix_tagging_constraints.rb
+++ db/migrate/20230717164740_fix_tagging_constraints.rb
@@ -1,5 +1,5 @@
 class FixTaggingConstraints < ActiveRecord::Migration[6.1]
   def change
-    execute 'ALTER TABLE taggings DROP CONSTRAINT taggings_manifestation_id_fk'
+    execute 'ALTER TABLE taggings DROP FOREIGN KEY taggings_manifestation_id_fk'
   end
 end
