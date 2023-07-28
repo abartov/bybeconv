120f9b5585d7f04395237db89839fe1342c5cf60
Asaf Bartov <asaf.bartov@gmail.com>
2023-07-28 19:09:54 +0300

    Switched to puma

---
+++ config/puma.rb
@@ -0,0 +1,25 @@
+app_dir = File.expand_path("../..", __FILE__)
+shared_dir = "#{app_dir}"
+rackup(File.expand_path('../config.ru', __dir__))
+if ENV['RACK_ENV'] == 'production'
+  require 'puma/daemon'
+  environment 'production'
+  workers Integer(ENV['WEB_CONCURRENCY'] || 5)
+  daemonize
+  bind "unix://#{shared_dir}/tmp/sockets/puma.sock"
+  stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+else
+  workers 0
+  environment 'development'
+end
+
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+threads 1, threads_count
+
+preload_app!
+
+# port        ENV['PORT']     || 3000
+stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+pidfile "#{shared_dir}/tmp/pids/puma.pid"
+
+
