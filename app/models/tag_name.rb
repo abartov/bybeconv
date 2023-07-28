64d47a06be52c9c78f819afc8c820b349ac1764d
Asaf Bartov <asaf.bartov@gmail.com>
2023-07-29 00:30:09 +0300

    Fixed autocomplete for tags in search

--- app/models/tag_name.rb
+++ app/models/tag_name.rb
@@ -1,3 +1,4 @@
 class TagName < ApplicationRecord
-  belongs_to :tag
+  belongs_to :tag, inverse_of: :tag_names
+  scope :approved, -> { joins(:tag).includes(:tag).where(tags: {status: Tag.statuses[:approved]}) }
 end
