08d2816acc5401997ca25e76812777539270f6e2
Asaf Bartov <asaf.bartov@gmail.com>
2023-09-03 10:31:15 +0300

    Added puma_worker_killer

--- Gemfile
+++ Gemfile
@@ -90,6 +90,7 @@ gem 'rswag-ui'
 gem 'ahoy_matey' # for recording events
 gem 'blazer' # for exploring Ahoy events
 gem 'puma'
+gem 'puma_worker_killer' # cycle workers when they bloat
 gem 'rack-attack' # control misbehaving clients
 
 ## these were used for some legacy HtmlDir VIAF lookup stuff. They have a huge RAM footprint (~160MB per process), so commented out until needed again.
--- config/puma.rb
+++ config/puma.rb
@@ -21,4 +21,18 @@ preload_app!
 # port        ENV['PORT']     || 3000
 pidfile "#{shared_dir}/tmp/pids/puma.pid"
 
+before_fork do
+  require 'puma_worker_killer'
+  PumaWorkerKiller.config do |config|
+    config.ram           = 4096 # mb
+    config.frequency     = 60    # seconds
+    config.percent_usage = 0.98
+    #config.rolling_restart_frequency = 12 * 3600 # 12 hours in seconds, or 12.hours if using Rails
+    #config.reaper_status_logs = true # setting this to false will not log lines like:
+    # PumaWorkerKiller: Consuming 54.34765625 mb with master and 2 workers.
 
+    #config.pre_term = -> (worker) { puts "Worker #{worker.inspect} being killed" }
+    #config.rolling_pre_term = -> (worker) { puts "Worker #{worker.inspect} being killed by rolling restart" }
+  end
+  PumaWorkerKiller.start
+end
