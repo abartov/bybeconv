09c96ff27f92bdb09c5c1c5478606e2fbf1fb6ff
Asaf Bartov <asaf.bartov@gmail.com>
2024-03-17 13:32:14 +0000

    blackisted an IP

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -3,7 +3,7 @@ class Rack::Attack
   safelist('allow-localhost') do |req|
     '127.0.0.1' == req.ip || '::1' == req.ip
   end
-
+  blocklist_ip("84.95.84.30")
   # Throttle requests to 5 requests per second per IP
   throttle('req/ip', limit: 300, period: 5.minutes) do |req|
     req.ip unless req.path.start_with?('/assets')
