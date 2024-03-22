59a8b3d722a4246744b754553e4a1f1eb8486203
Damir Sultanbekov <Damir.Sultanbekov@yandex.ru>
2024-03-22 20:35:08 +0500

    made email field mandatory in proof creation dialog. fixed bug in antispam check (proof creation passed even if check failed)

--- spec/controllers/proof_controller_spec.rb
+++ spec/controllers/proof_controller_spec.rb
@@ -18,4 +18,59 @@ describe ProofController do
       it { is_expected.to be_successful }
     end
   end
+
+  describe '#create' do
+    let(:email) { 'john.doe@test.com' }
+    let(:ziburit) { 'ביאליק' }
+    let(:errors) { assigns(:errors) }
+
+    let(:manifestation) { create(:manifestation) }
+
+    let(:params) do
+      {
+        from: email,
+        highlight: 'highlight text',
+        what: 'what text',
+        manifestation: manifestation.id,
+        ziburit: ziburit
+      }
+    end
+
+    subject(:call) { post :create, params: params, format: :js }
+
+    context 'when everything is OK' do
+      it { is_expected.to be_successful }
+      it 'creates new Proof record with given params' do
+        expect { call }.to change { Proof.count }.by(1)
+        expect(Proof.order(id: :desc).first).to have_attributes(
+          status: 'new',
+          from: email,
+          what: 'what text',
+          highlight: 'highlight text',
+          manifestation: manifestation
+        )
+      end
+    end
+
+    context 'when email is missing' do
+      let(:email) { ' ' }
+
+      it 'returns error' do
+        expect(call).to be_unprocessable
+        expect(JSON.parse(response.body)).to eq([I18n.t('proof.create.email_missing')])
+      end
+      it { expect { call }.not_to change { Proof.count } }
+    end
+
+    context 'when control question failed' do
+      let(:ziburit) { 'WRONG' }
+
+      it 'returns error' do
+        expect(call).to be_unprocessable
+        expect(JSON.parse(response.body)).to eq([I18n.t('proof.create.ziburit_failed')])
+      end
+
+      it { expect { call }.not_to change { Proof.count } }
+    end
+  end
 end
