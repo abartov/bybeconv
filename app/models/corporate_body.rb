85ccc7ff4c9703e0f341e7eaab282a595bae036b
Asaf Bartov <asaf.bartov@gmail.com>
2024-03-30 22:51:01 +0300

    Corporates can have profile images too

--- app/models/corporate_body.rb
+++ app/models/corporate_body.rb
@@ -1,4 +1,5 @@
 class CorporateBody < ApplicationRecord
   has_many :involved_authorities, class_name: 'InvolvedAuthority', as: :authority, dependent: :destroy
+  has_attached_file :profile_image, styles: { full: "720x1040", medium: "360x520", thumb: "180x260", tiny: "90x120"}, default_url: :placeholder_image_url, storage: :s3, s3_credentials: 'config/s3.yml', s3_region: 'us-east-1'
 
 end
