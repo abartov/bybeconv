8ca56628236781c52f83640a5ec4ed8e2d2b17a9
Damir Sultanbekov <Damir.Sultanbekov@yandex.ru>
2024-04-08 18:58:43 +0500

    moved manifestations/paginate partial to shared folder and added there some common js Extracted common logic for paging/filtering to FilteringAndPaginationConcern

---
+++ app/controllers/concerns/filtering_and_pagination_concern.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+# This concern contains common logic used to organize filtering and pagination
+module FilteringAndPaginationConcern
+  extend ActiveSupport::Concern
+
+  PAGE_SIZE = 100
+
+  private
+
+  def es_buckets_to_facet(buckets, codehash)
+    facet = {}
+    buckets.each do |facethash|
+      facet[codehash[facethash['key']]] = facethash['doc_count'] unless codehash[facethash['key']].nil?
+    end
+    facet
+  end
+
+  def paginate(collection)
+    @total = collection.count
+    @total_pages = (@total / PAGE_SIZE.to_f).ceil
+
+    # After we've swtiched to search_after logic for paging, page is only used to generate proper offset of item indices
+    # in works list (e.g. to start second page from index 101)
+    @page = (params[:page] || 1).to_i
+
+    @emit_filters = params[:load_filters] == 'true' || params[:emit_filters] == 'true'
+
+    fill_aggregated_facets(collection) # This method should be implemented in controllers using this concern
+
+    # checking if non-first page should be loaded
+    search_after_id = params[:search_after_id]
+    search_after_value = params[:search_after_value]
+    if search_after_id.present?
+      collection = collection.search_after(search_after_value, search_after_id)
+    end
+
+    if @reverse
+      # Fetching previous page
+      records = collection.limit(PAGE_SIZE).to_a
+      have_more_items = true # we know that there are more items after fetched page
+      records.reverse! # reordering items to display them in proper ordering
+    else
+      # Fetching next page
+      # we're retrieving one extra item to check if we have more items after fetched page
+      records = collection.limit(PAGE_SIZE + 1).to_a
+      have_more_items = records.size == PAGE_SIZE + 1
+      records = records[0..-2] if have_more_items # removing extra item
+    end
+
+    sort_column = get_sort_column(@sort_by)
+    @search_after_for_next = search_after_for_item(records.last, sort_column, false) if have_more_items
+    @search_after_for_previous = search_after_for_item(records.first, sort_column, true) if @page > 1
+
+    records
+  end
+
+  def search_after_for_item(item, sort_column, reverse)
+    {
+      value: item.send(sort_column),
+      id: item.id,
+      page: reverse ? @page - 1 : @page + 1,
+      reverse: reverse.to_s
+    }
+  end
+end
