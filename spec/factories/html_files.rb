8e5ece86a98e15a6dc6f600f9961915192b0ed9c
Damir Sultanbekov <Damir.Sultanbekov@yandex.ru>
2024-05-14 02:21:47 +0500

    Fixed HtmlFile.create_WEM_new method to work with new intellectual_property field

--- spec/factories/html_files.rb
+++ spec/factories/html_files.rb
@@ -1,5 +1,12 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :html_file do
-
+    status { 'Accepted' }
+    title { "#{Faker::Lorem.sentence}." }
+    genre { Work::GENRES.sample }
+    publisher { Faker::Company.name }
+    person { create(:person) }
+    translator { create(:person) }
   end
 end
