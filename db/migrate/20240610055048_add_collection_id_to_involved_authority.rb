eb700abeeb5db0650a785de9854cc3e93663f6ae
Asaf Bartov <asaf.bartov@gmail.com>
2024-06-14 16:40:54 +0300

    Tolerate existing column

--- db/migrate/20240610055048_add_collection_id_to_involved_authority.rb
+++ db/migrate/20240610055048_add_collection_id_to_involved_authority.rb
@@ -2,6 +2,6 @@
 
 class AddCollectionIdToInvolvedAuthority < ActiveRecord::Migration[6.1]
   def change
-    add_belongs_to :involved_authorities, :collection, type: :integer, null: true
+    add_belongs_to :involved_authorities, :collection, type: :integer, null: true, if_not_exists: true
   end
 end
