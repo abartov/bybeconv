f9aaa2dee4b6300a76a344ed305ccceedd882841
Damir Sultanbekov <Damir.Sultanbekov@yandex.ru>
2024-06-19 03:28:04 +0400

    Fixed some errors found after rebasing branch on master

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_06_10_055048) do
+ActiveRecord::Schema.define(version: 2024_06_14_064855) do
 
   create_table "aboutnesses", id: :integer, charset: "utf8mb4", collation: "utf8mb4_bin", force: :cascade do |t|
     t.integer "work_id"
