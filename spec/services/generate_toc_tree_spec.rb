e8634d51ca8cb3588a623901d37758f857c6acb5
Damir Sultanbekov <Damir.Sultanbekov@yandex.ru>
2024-08-07 03:15:15 +0500

    modified generate_toc_tree service to take in account seqno ordering
    fixed broken collection's specs

--- spec/services/generate_toc_tree_spec.rb
+++ spec/services/generate_toc_tree_spec.rb
@@ -14,63 +14,35 @@ describe GenerateTocTree do
   end
 
   context 'when there are works' do
-    let!(:other_authority) { create(:authority) }
+    include_context 'when authority has several collections'
 
-    let!(:top_level_collection) { create(:collection) }
-    let!(:top_level_collection_with_nested_collections) do
-      create(
-        :collection,
-        authors: [other_authority],
-        included_collections: [nested_edited_collection, nested_translated_collection]
-      )
-    end
-    let!(:uncollected_collection) { create(:collection, collection_type: :uncollected) }
-    let!(:nested_edited_collection) { create(:collection, editors: [authority]) }
-    let!(:nested_translated_collection) { create(:collection, translators: [authority]) }
-    let!(:edited_manifestations) do
-      create_list(
-        :manifestation,
-        3,
-        collections: [nested_edited_collection],
-        editor: authority,
-        author: other_authority
-      )
-    end
-    # For translated manifestations we don's specify authority on manifestaton level
-    let!(:translated_manifestations) do
-      create_list(
-        :manifestation,
-        2,
-        collections: [nested_translated_collection],
-        author: other_authority
-      )
-    end
-
-    let!(:uncollected_manifestations) do
-      create_list(:manifestation, 2, author: authority, collections: [uncollected_collection])
-    end
-
-    let!(:top_level_manifestations) do
-      create_list(:manifestation, 3, author: authority, collections: [top_level_collection])
-    end
-
-    let(:top_level_node) { result.find { |n| n.item == top_level_collection } }
-    let(:top_level_with_nested_node) { result.find { |n| n.item == top_level_collection_with_nested_collections } }
-    let(:uncollected_node) { result.find { |n| n.item == uncollected_collection } }
-    let(:nested_edited_node) { top_level_with_nested_node.children.find { |n| n.item == nested_edited_collection } }
-    let(:nested_translated_node) do
-      top_level_with_nested_node.children.find { |n| n.item == nested_translated_collection }
-    end
+    let(:top_level_node) { find_node(result, top_level_collection) }
+    let(:top_level_with_nested_node) { find_node(result, top_level_collection_with_nested_collections) }
+    let(:uncollected_node) { find_node(result, uncollected_collection) }
+    let(:nested_edited_node) { find_child_node(top_level_with_nested_node, nested_edited_collection) }
+    let(:nested_translated_node) { find_child_node(top_level_with_nested_node, nested_translated_collection) }
 
     it 'runs successfully' do
       expect(result).to contain_exactly top_level_node, top_level_with_nested_node, uncollected_node
 
-      expect(top_level_node.children).to match_array top_level_manifestations
-      expect(top_level_with_nested_node.children.map(&:item)).to contain_exactly nested_translated_collection,
-                                                                                 nested_edited_collection
+      expect(top_level_node.children.map(&:first)).to match_array top_level_manifestations
+      expect(
+        top_level_with_nested_node.children.map { |c| c.first.item}
+      ).to contain_exactly nested_translated_collection,
+                           nested_edited_collection
 
-      expect(nested_edited_node.children).to match_array edited_manifestations
+      expect(nested_edited_node.children.map(&:first)).to match_array edited_manifestations
       expect(nested_translated_node.children).to be_empty
     end
+  end
+
+  private
+
+  def find_node(nodes, item)
+    nodes.find { |n| n.item == item }
+  end
+
+  def find_child_node(parent_node, item)
+    find_node(parent_node.children.map(&:first), item)
   end
 end
---
+++ spec/support/shared_examples/collection_toc.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+RSpec.shared_context 'when authority has several collections' do
+  let!(:authority) { create(:authority, uncollected_works_collection: uncollected_collection) }
+  let!(:other_authority) { create(:authority) }
+
+  let!(:top_level_collection) { create(:collection) }
+  let!(:top_level_collection_with_nested_collections) do
+    create(
+      :collection,
+      authors: [other_authority],
+      included_collections: [nested_edited_collection, nested_translated_collection]
+    )
+  end
+  let!(:uncollected_collection) { create(:collection, collection_type: :uncollected) }
+  let!(:nested_edited_collection) { create(:collection, editors: [authority]) }
+  let!(:nested_translated_collection) { create(:collection, translators: [authority]) }
+  let!(:edited_manifestations) do
+    create_list(
+      :manifestation,
+      3,
+      collections: [nested_edited_collection],
+      editor: authority,
+      author: other_authority
+    )
+  end
+  # For translated manifestations we don's specify authority on manifestaton level
+  let!(:translated_manifestations) do
+    create_list(
+      :manifestation,
+      2,
+      collections: [nested_translated_collection],
+      author: other_authority
+    )
+  end
+
+  let!(:uncollected_manifestations) do
+    create_list(:manifestation, 2, author: authority, collections: [uncollected_collection])
+  end
+
+  let!(:top_level_manifestations) do
+    create_list(:manifestation, 3, author: authority, collections: [top_level_collection])
+  end
+end
