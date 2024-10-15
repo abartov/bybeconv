5c2ed6b0efff445800bd594420e375658350b6d4
Asaf Bartov <asaf.bartov@gmail.com>
2024-10-15 20:31:16 +0300

    vuln scanner IP

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -4,6 +4,8 @@ class Rack::Attack
     '127.0.0.1' == req.ip || '::1' == req.ip
   end
   blocklist_ip("84.95.84.30")
+  blocklist_ip("147.189.171.165") # Oct 2024
+
   # Throttle requests to 5 requests per second per IP
   throttle('req/ip', limit: 500, period: 3.minutes) do |req|
     req.ip unless req.path.start_with?('/assets')
