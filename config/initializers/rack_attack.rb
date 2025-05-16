996f09ec8ef02bda361f48b653aad3c15aa4e3ef
Asaf Bartov <asaf.bartov@gmail.com>
2025-05-16 17:14:22 +0300

    Rack-attack improvement

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -3,7 +3,10 @@ class Rack::Attack
   safelist('allow-localhost') do |req|
     '127.0.0.1' == req.ip || '::1' == req.ip
   end
-  blocklist_ip("84.95.84.30")
+  BAD_IPS = ["84.95.84.30", "147.189.171.165", "20.171.0.0/16"]
+  Rack::Attack.blocklist "Block IPs from Environment Variable" do |req|
+    BAD_IPS.include?(req.ip) || req.path.index('&gt&gt')
+  end
   # Throttle requests to 5 requests per second per IP
   throttle('req/ip', limit: 500, period: 3.minutes) do |req|
     req.ip unless req.path.start_with?('/assets')
