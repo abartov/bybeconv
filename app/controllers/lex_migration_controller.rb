8c6659b657b3b926056eaeead1a5c3c3e123add4
Asaf Bartov <asaf.bartov@gmail.com>
2025-06-14 20:13:18 +0300

    lint etc.

--- app/controllers/lex_migration_controller.rb
+++ app/controllers/lex_migration_controller.rb
@@ -7,11 +7,16 @@ class LexMigrationController < ApplicationController
   end
 
   def migrate_person
+    if params[:id].blank?
+      redirect_to action: :list_files, notice: 'No file selected for migration.'
+      return
+    end
     file = LexFile.find(params[:id])
     if file.nil?
       redirect_to action: :index
     else
-      @lex_entry = LexEntry.new(title: file.title, sort_title: file.title.strip_nikkud.tr('-־[]()*"\'', '').strip, status: :raw)
+      @lex_entry = LexEntry.new(title: file.title, sort_title: file.title.strip_nikkud.tr('-־[]()*"\'', '').strip,
+                                status: :raw)
       @lex_entry.save!
       file.lex_entry = @lex_entry
       file.status_ingested!
