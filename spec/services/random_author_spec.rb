fbd262c7077a4c199c1ffe4fea67b64f800a1844
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2025-06-19 13:27:54 +0500

    Added clean_tables call to RandomAuthor spec

--- spec/services/random_author_spec.rb
+++ spec/services/random_author_spec.rb
@@ -5,6 +5,7 @@ require 'rails_helper'
 describe RandomAuthor do
   describe '#call' do
     before do
+      clean_tables
       create_list(:manifestation, 5, status: :published, genre: :drama)
       create_list(:manifestation, 5, status: :published, genre: :poetry)
       create_list(:manifestation, 5, status: :published, genre: :lexicon)
