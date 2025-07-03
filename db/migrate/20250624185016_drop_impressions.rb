1994c2b32b9e5d362e819c5bf36b00e978ed4372
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2025-07-03 15:52:53 +0500

    fixed DropImpressions migration to handle duplicated entries in YearTotals

--- db/migrate/20250624185016_drop_impressions.rb
+++ db/migrate/20250624185016_drop_impressions.rb
@@ -4,9 +4,17 @@ class DropImpressions < ActiveRecord::Migration[6.1]
   def change
     execute <<~SQL
         insert into year_totals (item_type, item_id, year, total, event, created_at, updated_at)
-        select impressionable_type, impressionable_id, year(updated_at) as year, count(*), 'view', current_timestamp(), current_timestamp() 
-        from impressions 
-        group by impressionable_id, impressionable_type, year(updated_at)
+        select impressionable_type, impressionable_id, year, total_cnt, event, current_timestamp(), current_timestamp() from     
+            (
+                select
+                    impressionable_type, impressionable_id, year(updated_at) as year, count(*) as total_cnt, 'view' as event 
+                from
+                    impressions 
+                group by
+                    impressionable_id, impressionable_type, year(updated_at)
+            ) t
+        on duplicate key update
+            total = total + t.total_cnt, updated_at = current_timestamp()
     SQL
 
     drop_table :impressions
