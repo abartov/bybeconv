89a10833cdf41d2c83a8a99fdbb12819be6c6744
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2025-07-28 15:13:39 +0500

    rolled back linter changes to person.rb

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,27 +1,33 @@
 include BybeUtils
 class Person < ApplicationRecord
-  enum gender: { male: 0, female: 1, other: 2, unknown: 3 }
-  enum period: { ancient: 0, medieval: 1, enlightenment: 2, revival: 3, modern: 4 }
+  enum gender: %i(male female other unknown)
+  enum period: %i(ancient medieval enlightenment revival modern)
   has_one :authority, inverse_of: :person, dependent: :destroy
 
   scope :with_name, -> { joins(:authority).select('people.*, authorities.name') }
 
   has_paper_trail
   def died_years_ago
-    dy = death_year.to_i
-    return Date.today.year - dy
-  rescue StandardError
-    return 0
+    begin
+      dy = death_year.to_i
+      return Date.today.year - dy
+    rescue
+      return 0
+    end
   end
 
   def birth_year
     return '?' if birthdate.nil?
-
     bpos = birthdate.strip_hebrew.index('-') # YYYYMMDD or YYYY is assumed
-    return birthdate[0..bpos - 1].strip unless bpos.nil?
-    return ::Regexp.last_match(0) if birthdate =~ /\d\d\d+/
-
-    return birthdate
+    if bpos.nil?
+      if birthdate =~ /\d\d\d+/
+        return $&
+      else
+        return birthdate
+      end
+    else
+      return birthdate[0..bpos-1].strip
+    end
   end
 
   def gender_letter
@@ -34,12 +40,16 @@ class Person < ApplicationRecord
 
   def death_year
     return '?' if deathdate.nil?
-
     dpos = deathdate.strip_hebrew.index('-')
-    return deathdate[0..dpos - 1].strip unless dpos.nil?
-    return ::Regexp.last_match(0) if deathdate =~ /\d\d\d+/
-
-    return deathdate
+    if dpos.nil?
+      if deathdate =~ /\d\d\d+/
+        return $&
+      else
+        return deathdate
+      end
+    else
+      return deathdate[0..dpos-1].strip
+    end
   end
 
   def life_years
@@ -48,7 +58,6 @@ class Person < ApplicationRecord
 
   def period_string
     return '' if period.nil?
-
     return t(period)
   end
 end
