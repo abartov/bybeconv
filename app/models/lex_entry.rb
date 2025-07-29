a71607ec9b687656a91271810bccad0e1f8f2d5b
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2025-07-29 17:07:48 +0500

    fixed compatibility problems with a new rails versions

--- app/models/lex_entry.rb
+++ app/models/lex_entry.rb
@@ -1,5 +1,5 @@
 class LexEntry < ApplicationRecord
   has_one :lex_file
-  belongs_to :lex_item, polymorphic: true # this can be LexPerson or LexPublication (or...?)
+  belongs_to :lex_item, polymorphic: true, optional: true # this can be LexPerson or LexPublication (or...?)
   enum status: { raw: 0, migrated: 1, manual: 2, deprecated: 3 } # raw = not done migrating; migrated = should no longer be served from static PHP
 end
