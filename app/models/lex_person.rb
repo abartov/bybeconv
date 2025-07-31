e4795c3a3f7dae21932ada0892e77b97d04b8ada
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2025-07-31 14:08:13 +0500

    CRUD for LexPerson Added validation to LexEntry.title

--- app/models/lex_person.rb
+++ app/models/lex_person.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
+# Person from Lexicon
 class LexPerson < ApplicationRecord
-  @@html_entities_coder = HTMLEntities.new
-  has_one :lex_entry
+  has_one :entry, as: :lex_item, class_name: 'LexEntry', dependent: :destroy
   has_many :lex_links, as: :item, dependent: :destroy
   belongs_to :person, optional: true
-end
 
-# ["Books"]=>4325, ["Bib."]=>3896, ["links"]=>3879, ["top"]=>179, ["no1"]=>67, ["no2"]=>65, ["no3"]=>52, ["no14-15"]=>3, ["no18"]=>8, ["no19"]=>7, ["no20"]=>6, ["Bib.1"]=>3, ["no410"]=>1, ["no411"]=>1, ["no412"]=>1, ["HLN3"]=>3, ["no0101"]=>2, ["no0102"]=>2, ["no010304"]=>2, ["no010506"]=>2, ["no21-22"]=>1, ["no307"]=>2, ["no308"]=>2, ["no309-310"]=>1, ["no311"]=>1, ["no312-313"]=>1, ["no314"]=>2, ["no315"]=>2, ["no316"]=>2, ["no360"]=>4, ["no359"]=>12, ["5552"]=>1, ["5261"]=>3, ["5472"]=>1, ["5502"]=>1, ["5502\" href=\"01227.php"]=>1, ["5433"]=>1, ["5162"]=>3, ["5350"]=>3, ["5350\" href=\"00386.php"]=>1, ["5352"]=>3, ["5352\" href=\"00386.php"]=>1, ["5345"]=>3, ["5
+  accepts_nested_attributes_for :entry, update_only: true
+end
