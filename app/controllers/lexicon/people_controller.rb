347b230f969faf0a317f67845050885a76647a86
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2025-07-31 14:08:13 +0500

    CRUD for LexPerson Added validation to LexEntry.title

---
+++ app/controllers/lexicon/people_controller.rb
@@ -0,0 +1,76 @@
+# frozen_string_literal: true
+
+module Lexicon
+  # Controller to work with People records in Lexicon
+  class PeopleController < ::ApplicationController
+    before_action :set_lex_person, only: %i(show edit update destroy)
+
+    # GET /lex_people or /lex_people.json
+    def index
+      @lex_people = LexPerson.all
+    end
+
+    # GET /lex_people/1 or /lex_people/1.json
+    def show; end
+
+    # GET /lex_people/new
+    def new
+      @lex_person = LexPerson.new
+      @lex_person.build_entry
+    end
+
+    # GET /lex_people/1/edit
+    def edit; end
+
+    # POST /lex_people or /lex_people.json
+    def create
+      @lex_person = LexPerson.new(lex_person_params)
+      @lex_person.entry.status = :manual
+
+      if @lex_person.save
+        redirect_to lexicon_person_path(@lex_person), notice: t('.success')
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+    # PATCH/PUT /lex_people/1 or /lex_people/1.json
+    def update
+      if @lex_person.update(lex_person_params)
+        redirect_to lexicon_person_path(@lex_person), notice: t('.success')
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+
+    # DELETE /lex_people/1 or /lex_people/1.json
+    def destroy
+      LexPerson.transaction do
+        @lex_person.destroy
+      end
+      redirect_to lexicon_people_path, alert: t('.success')
+    end
+
+    private
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_lex_person
+      @lex_person = LexPerson.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def lex_person_params
+      params.require(:lex_person)
+            .permit(
+              :aliases,
+              :copyrighted,
+              :birthdate,
+              :deathdate,
+              :bio,
+              :works,
+              :about,
+              entry_attributes: [:title]
+            )
+    end
+  end
+end
--- spec/factories/lex_people.rb
+++ spec/factories/lex_people.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :lex_person do
-    aliases { "MyString" }
-    copyrighted { false }
-    birthdate { "MyString" }
-    deathdate { "MyString" }
-    bio { "MyText" }
-    works { "MyText" }
-    about { "MyText" }
+    aliases { Faker::Name.name }
+    copyrighted { [true, false].sample }
+    birthdate { Faker::Date.birthday(min_age: 50, max_age: 100).to_fs }
+    deathdate { Faker::Date.birthday(min_age: 80, max_age: 0).to_fs }
+    bio { Faker::Lorem.paragraph }
+    works { Faker::Lorem.paragraph }
+    about { Faker::Lorem.sentence }
   end
 end
