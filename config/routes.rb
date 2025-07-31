3d983cc4f603a6d64250ff332956696406b4a5e3
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2025-07-31 14:08:13 +0500

    CRUD for LexPerson Added validation to LexEntry.title

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,10 @@ Bybeconv::Application.routes.draw do
     end
   end
 
+  namespace :lexicon do
+    resources :people
+  end
+
   resources :ingestibles do
     resources :authorities, controller: :ingestible_authorities, only: %i(create destroy) do
       member do
@@ -72,7 +76,6 @@ Bybeconv::Application.routes.draw do
   get 'lex_migration/resolve_entry'
 
   resources :lex_files
-  resources :lex_people
   resources :lex_entries
   resources :lex_links
   resources :lex_citations
