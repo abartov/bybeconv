aab619bcd91793d54821edde675f3fe6b193d587
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2025-08-05 16:07:34 +0500

    fixed errors in IngestPerson service caused by Rails version upgrade (mandatory belongs_to)

--- spec/factories/lex_files.rb
+++ spec/factories/lex_files.rb
@@ -1,9 +1,13 @@
 FactoryBot.define do
   factory :lex_file do
-    fname { "MyString" }
-    status { 1 }
-    title { "MyString" }
-    entrytype { 1 }
-    comments { "MyText" }
+    status { 'classified' }
+    comments { Faker::Lorem.sentence }
+
+    trait :person do
+      fname { "/#{format('%05d', Faker::Number.between(from: 1, to: 99999))}.php" }
+      entry_type { 'person' }
+      title { Faker::Name.name }
+    end
+
   end
 end
