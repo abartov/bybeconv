a470568e97c818c47371286f889e2b623984e2f5
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2025-08-05 22:13:50 +0500

    fixed linter warnings in a rake task

--- app/models/lex_issue.rb
+++ app/models/lex_issue.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class LexIssue < ApplicationRecord
   belongs_to :lex_publication
 end
--- app/views/lex_entries/index.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.array! @lex_entries, partial: "lex_entries/lex_entry", as: :lex_entry
