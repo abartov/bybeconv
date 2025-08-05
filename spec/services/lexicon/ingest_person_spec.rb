6a555d3001ff89284d6f64bd6b34668157a3985e
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2025-08-05 18:14:12 +0500

    Added parsing of birht/death years for LexPerson

--- spec/services/lexicon/ingest_person_spec.rb
+++ spec/services/lexicon/ingest_person_spec.rb
@@ -5,23 +5,51 @@ require 'rails_helper'
 describe Lexicon::IngestPerson do
   subject(:call) { described_class.call(file.id) }
 
-  let!(:file) do
-    create(
-      :lex_file,
-      {
-        entrytype: :person,
-        status: :classified,
-        title: 'Gabriella Avigur',
-        fname: '/00002.php',
-        full_path: Rails.root.join('spec', 'data', 'lexicon', '00002.php')
-      }
-    )
+  context 'when birthdate only is provided' do
+    let!(:file) do
+      create(
+        :lex_file,
+        {
+          entrytype: :person,
+          status: :classified,
+          title: 'Gabriella Avigur',
+          fname: '/00002.php',
+          full_path: Rails.root.join('spec/data/lexicon/00002.php')
+        }
+      )
+    end
+
+    it 'parses file successfully' do
+      expect { call }.to change(LexEntry, :count).by(1).and change(LexPerson, :count).by(1)
+      expect(file.reload).to be_status_ingested
+
+      person = file.lex_entry.lex_item
+      expect(person).to be_an_instance_of(LexPerson)
+      expect(person).to have_attributes(birthdate: '1946', deathdate: nil)
+    end
   end
 
-  it 'parses file successfully' do
-    expect { call }.to change { LexEntry.count }.by(1).and change { LexPerson.count }.by(1)
+  context 'when both birthdate and deathdate provided' do
+    let(:file) do
+      create(
+        :lex_file,
+        {
+          entrytype: :person,
+          status: :classified,
+          title: 'Samuel Bass',
+          fname: '/00024.php',
+          full_path: Rails.root.join('spec/data/lexicon/00024.php')
+        }
+      )
+    end
 
-    expect(file.reload).to be_status_ingested
+    it 'parses file successfully' do
+      expect { call }.to change(LexEntry, :count).by(1).and change(LexPerson, :count).by(1)
+      expect(file.reload).to be_status_ingested
 
+      person = file.lex_entry.lex_item
+      expect(person).to be_an_instance_of(LexPerson)
+      expect(person).to have_attributes(birthdate: '1899', deathdate: '1949')
+    end
   end
 end
