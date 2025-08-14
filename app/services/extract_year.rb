2b22cdfcca309e23e1e2ef32249a9a9b965eba0a
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2025-08-14 17:42:10 +0500

    Extracted methods to work with life dates from Person class to LifePeriod concern.

---
+++ app/services/extract_year.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# Extracts year from the string representing date
+class ExtractYear < ApplicationService
+  # @param datestr string representing date to be parsed
+  # @param default value to return if year cannot be extracted
+  def call(datestr, default = '?')
+    return default if datestr.blank?
+
+    pos = datestr.strip_hebrew.index('-') # YYYYMMDD or YYYY is assumed
+    return datestr[0..(pos - 1)].strip unless pos.nil?
+    return ::Regexp.last_match(0) if datestr =~ /\d\d\d+/
+
+    return datestr
+  end
+end
---
+++ spec/models/concerns/life_period_spec.rb
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe LifePeriod do
+  before do
+    stub_const(
+      'Model',
+      Class.new do
+        include LifePeriod
+
+        attr_accessor :birthdate, :deathdate
+
+        def initialize(birthdate, deathdate)
+          @birthdate = birthdate
+          @deathdate = deathdate
+        end
+      end
+    )
+  end
+
+  let(:model) { Model.new(birthdate, deathdate) }
+  let(:birthdate) { '1923-01-22' }
+  let(:deathdate) { '1976-05-12' }
+
+  describe '.death_year' do
+    subject { model.death_year }
+
+    it { is_expected.to eq('1976') }
+
+    context 'when deathdate is nil' do
+      let(:deathdate) { nil }
+
+      it { is_expected.to eq('?') }
+    end
+  end
+
+  describe '.birth_year' do
+    subject { model.birth_year }
+
+    it { is_expected.to eq('1923') }
+
+    context 'when birthdate is blank' do
+      let(:birthdate) { '   ' }
+
+      it { is_expected.to eq('?') }
+    end
+  end
+
+  describe '.life_years' do
+    subject { model.life_years }
+
+    it { is_expected.to eq('1923&rlm;-1976') }
+
+    context 'when deathdate is nil' do
+      let(:deathdate) { nil }
+
+      it { is_expected.to eq('1923&rlm;-?') }
+    end
+
+    context 'when birthdate is nil' do
+      let(:birthdate) { nil }
+
+      it { is_expected.to eq('?&rlm;-1976') }
+    end
+
+    context 'when both dates are nil' do
+      let(:birthdate) { nil }
+      let(:deathdate) { nil }
+
+      it { is_expected.to eq('?&rlm;-?') }
+    end
+  end
+end
