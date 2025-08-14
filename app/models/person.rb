17a0c3398dad6e66c1d5c5bd0023c50893c1cf42
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2025-08-14 17:42:10 +0500

    Extracted methods to work with life dates from Person class to LifePeriod concern.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,7 @@
 include BybeUtils
 class Person < ApplicationRecord
+  include LifePeriod
+
   enum :gender, { male: 0, female: 1, other: 2, unknown: 3 }
   enum :period, { ancient: 0, medieval: 1, enlightenment: 2, revival: 3, modern: 4 }
   has_one :authority, inverse_of: :person, dependent: :destroy
@@ -7,28 +9,6 @@ class Person < ApplicationRecord
   scope :with_name, -> { joins(:authority).select('people.*, authorities.name') }
 
   has_paper_trail
-  def died_years_ago
-    begin
-      dy = death_year.to_i
-      return Date.today.year - dy
-    rescue
-      return 0
-    end
-  end
-
-  def birth_year
-    return '?' if birthdate.nil?
-    bpos = birthdate.strip_hebrew.index('-') # YYYYMMDD or YYYY is assumed
-    if bpos.nil?
-      if birthdate =~ /\d\d\d+/
-        return $&
-      else
-        return birthdate
-      end
-    else
-      return birthdate[0..bpos-1].strip
-    end
-  end
 
   def gender_letter
     return gender == 'female' ? 'ה' : 'ו'
@@ -36,24 +16,6 @@ class Person < ApplicationRecord
 
   def blog_count
     return 0 # TODO: implement
-  end
-
-  def death_year
-    return '?' if deathdate.nil?
-    dpos = deathdate.strip_hebrew.index('-')
-    if dpos.nil?
-      if deathdate =~ /\d\d\d+/
-        return $&
-      else
-        return deathdate
-      end
-    else
-      return deathdate[0..dpos-1].strip
-    end
-  end
-
-  def life_years
-    return "#{birth_year}&rlm;-#{death_year}"
   end
 
   def period_string
