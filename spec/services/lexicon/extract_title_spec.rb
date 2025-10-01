0e67b5b9c282d8158a6c9bbcb9556ab1b530d12e
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2025-10-02 02:03:40 +0500

    Updated extract_title service to use Nokogiri instead of regex

--- spec/services/lexicon/extract_title_spec.rb
+++ spec/services/lexicon/extract_title_spec.rb
@@ -8,12 +8,18 @@ describe Lexicon::ExtractTitle do
   context 'when person file is provided' do
     let(:file) { Rails.root.join('spec/data/lexicon/00024.php') }
 
-    it { is_expected.to eq('שמואל בס') }
+    it { is_expected.to eq('שמואל בס (1899־1949)') }
+
+    context 'when title is written in several spans' do
+      let(:file) { Rails.root.join('spec/data/lexicon/tsifroni.php') }
+
+      it { is_expected.to eq('גבריאל צפרוני (1915־2011)') }
+    end
   end
 
   context 'when publication file is provided' do
     let(:file) { Rails.root.join('spec/data/lexicon/02645001.php') }
 
-    it { is_expected.to eq('אליעזר ירושלמי') }
+    it { is_expected.to eq('אליעזר ירושלמי (1900–1962)') }
   end
 end
